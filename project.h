66cfe2313f61134fdd80d0f6d192e6095611f3b7
byarbrough <yarbrough.b@husky.neu.edu>
2017-11-14 16:19:07 -0500

    needed to be tracked

---
+++ project.h
@@ -0,0 +1,14 @@
+#ifndef PROJECT_H
+#define PROJECT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N 10000
+#define D 10
+#define K 3
+#define DATA_FILE "samples.csv"
+#define LABEL_FILE "clusters.csv"
+
+
+#endif
