f1b397f19d2fcca8b018769247c928c06e0ff604
CooperL <cooper.loughlin@gmail.com>
2017-11-16 17:45:00 -0500

    added MPI

--- omp_mpi/kmeans.c
+++ omp_mpi/kmeans.c
@@ -42,31 +42,64 @@ int main(int argc, char* argv[]) {
     int taskid, numtasks;
 
     // split into k nodes
+    MPI_Status status;
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
     MPI_Comm_rank(MPI_COMM_WORLD, &taskid);
 
     int num_iterations = 0;
+    int not_done = 0;
     do {
-        // split onto k nodes... later
-
+        printf("beginnign of loop iter: %d, taskid: %d\n",num_iterations,taskid);
         // calculate distance in each row
-        for(taskid = 0; taskid < K; taskid++)
-            calc_distance(data_arry,mu,dist,taskid);
-
+        //for(taskid = 0; taskid < K; taskid++)
+        calc_distance(data_arry,mu,dist,taskid);
+       
         // Send distances back to captain node
+        if (taskid != MASTER) {
+            MPI_Send(dist[taskid], N, MPI_FLOAT, MASTER, taskid, MPI_COMM_WORLD );
+        }
+        else {
+            // because MASTER is 0
+            for (i = 1; i < K; i++)
+                MPI_Recv(dist[i], N, MPI_FLOAT, i, i, MPI_COMM_WORLD, &status );
+        }
 
-        // select minimum distance and assign new label
-        find_min_dist(labels, dist);
+        if (taskid == MASTER) {
+            // select minimum distance and assign new label
+            find_min_dist(labels, dist);
     
-        // update mean
-        copy_mu(mu, mu_prev);
-        calc_mean(data_arry, labels, mu);
+            // update mean
+            copy_mu(mu, mu_prev);
+            calc_mean(data_arry, labels, mu);
     
-        num_iterations++;
+            // Send updated means back to nodes
+            for (i = 1; i < K; i++)
+                MPI_Send(mu[i], D, MPI_FLOAT, i, i, MPI_COMM_WORLD);
+        
+            num_iterations++;
+            printf("iteration: %d\n",num_iterations);
+
+            not_done = num_iterations < MAX_ITERATIONS && comp_dmu(mu, mu_prev);
+        }
+        else {
+            printf("taskid: %d\n", taskid);
+            MPI_Recv(mu[taskid], D, MPI_FLOAT, MASTER, taskid, MPI_COMM_WORLD, &status);
+            printf("after recieve\n");
+        }
+
+        if (taskid == MASTER) {
+            for(i = 1; i < K; i++)
+                MPI_Send(mu_prev[i], D, MPI_FLOAT, i, i, MPI_COMM_WORLD);
+        }
+        else {
+            MPI_Recv(mu_prev[taskid], D, MPI_FLOAT, MASTER, taskid, MPI_COMM_WORLD, &status);
+        }  
+        MPI_Bcast(&not_done, 1, MPI_INT, MASTER, MPI_COMM_WORLD);  
     }
-    while (num_iterations < MAX_ITERATIONS && comp_dmu(mu, mu_prev));
-   
+    while (not_done);
+
+    printf("after while taskid: %d\n",taskid); 
     MPI_Finalize();
 
     stop = CLOCK();
